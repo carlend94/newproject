b59fc97680f1355973c03986e740fa64b6d9c8b8
YAro <carlend94@ukr.net>
2017-12-27 01:58:32 +0200

    add last changes

--- client/carland/src/app/sto/sto.component.ts
+++ client/carland/src/app/sto/sto.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../services/authentication.service";
 
 @Component({
   selector: 'app-sto',
@@ -6,11 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sto.component.scss']
 })
 export class StoComponent implements OnInit {
-  isClassVisible: boolean = false;
+  isClassVisible: boolean = true;
   isClassVisible2: boolean = false;
   isClassVisible3: boolean = false;
   isClassVisible4: boolean = false;
-  constructor() { }
+  yourData: string = "Ваши данные";
+  isHover: boolean = false;
+  constructor( private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
@@ -20,18 +23,21 @@ export class StoComponent implements OnInit {
     this.isClassVisible2 = false;
     this.isClassVisible3 = false;
     this.isClassVisible4 = false;
+    this.yourData = "Ваши данные";
   }
   makeActive2() {
     this.isClassVisible = false;
     this.isClassVisible2 = true;
     this.isClassVisible3 = false;
     this.isClassVisible4 = false;
+    this.yourData = "Главная страница";
   }
   makeActive3() {
     this.isClassVisible = false;
     this.isClassVisible2 = false;
     this.isClassVisible3 = true;
     this.isClassVisible4 = false;
+    this.yourData = "Заявки на ремонт";
 
   }
 
@@ -40,5 +46,17 @@ export class StoComponent implements OnInit {
     this.isClassVisible2 = false;
     this.isClassVisible3 = false;
     this.isClassVisible4 = true;
+    this.yourData = "Карта всех СТО";
+  }
+
+  onHovering() {
+    this.isHover = true;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+  }
+  onUnovering() {
+    this.isHover = false;
   }
 }
